cbef6362cff7ec4a712e25333cec1336a8f27efc
Kobe McKee <kobe.mckee@gmail.com>
2019-11-22 13:01:32 -0700

    commit

---
+++ Assets/Scripts/TemplarKnightController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequiredComponent(typeof(SpriteRenderer), typeof(Rigidbody2D), typeof(Animator))]
+
+public class TemplarKnightController : MonoBehaviour
+{
+    public int playerHealth;
+
+    public float speed = 14f;
+    public float accel = 6f;
+    private Vector2 input;
+    private SpriteRenderer sr;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+
+    void Awake() {
+        sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+        input.x = Input.GetAxis("Horizontal");
+
+        if (input.x > 0f) {
+            sr.flipX = false;
+        } else if (input.x < 0f) {
+            sr.flipX = true;
+        }
+    }
+
+
+    void FixedUpdate() {
+
+        var acceleration = accel;
+        var xVelocity = 0f;
+
+        if (input.x == 0) {
+            xVelocity = 0f;
+        } else {
+            xVelocity = rb.velocity.x;
+        }
+
+        rb.AddForce(new Vector2(((input.x * speed) - rb.velocity.x) * acceleration, 0));
+
+        rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+    }
+
+
+}
+
